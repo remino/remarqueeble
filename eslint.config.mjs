a5714fb772e2bd4767ef25ea8e12962890837dee
Rémino Rem <29999+remino@users.noreply.github.com>
2026-06-28 19:27:38 +0900

    Add ESLint

---
+++ eslint.config.mjs
@@ -0,0 +1,61 @@
+import js from '@eslint/js'
+import astroParser from 'astro-eslint-parser'
+import astroPlugin from 'eslint-plugin-astro'
+import prettier from 'eslint-config-prettier'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+
+const toArray = value => (Array.isArray(value) ? value : value ? [value] : [])
+
+export default [
+	{
+		ignores: [
+			'.astro/**',
+			'deploy/**',
+			'dist/**',
+			'docs/**',
+			'node_modules/**',
+			'specs/**',
+		],
+	},
+	...toArray(js.configs.recommended),
+	...toArray(tseslint.configs.recommended),
+	...toArray(astroPlugin.configs['flat/recommended']),
+	{
+		files: ['**/*.{js,mjs,cjs}'],
+		languageOptions: {
+			globals: globals.node,
+			sourceType: 'module',
+		},
+	},
+	{
+		files: ['src/lib/**/*.ts'],
+		languageOptions: {
+			globals: globals.browser,
+		},
+	},
+	{
+		files: ['src/**/*.astro'],
+		languageOptions: {
+			parser: astroParser,
+			parserOptions: {
+				extraFileExtensions: ['.astro'],
+				parser: tseslint.parser,
+			},
+			globals: {
+				...globals.browser,
+				...globals.node,
+			},
+		},
+	},
+	{
+		rules: {
+			'no-unused-vars': 'off',
+			'@typescript-eslint/no-unused-vars': [
+				'warn',
+				{ argsIgnorePattern: '^_', ignoreRestSiblings: true },
+			],
+		},
+	},
+	prettier,
+]
